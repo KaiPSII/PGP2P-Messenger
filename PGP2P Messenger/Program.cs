ed3f9d2a7df8c4cdc08d179a07bf2ddf6c675fa0
Kai Stevenson <kai@kaistevenson.com>
2020-10-28 13:38:14 -0700

    Add project files.

---
+++ PGP2P Messenger/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PGP2P_Messenger
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
