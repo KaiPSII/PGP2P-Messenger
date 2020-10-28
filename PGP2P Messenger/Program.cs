92414e7724798a4c7cf6f4c4674c1567f2a4dd9b
Kai Stevenson <kai@kaistevenson.com>
2020-10-28 14:25:08 -0700

    added basic interface

--- PGP2P Messenger/Program.cs
+++ PGP2P Messenger/Program.cs
@@ -6,7 +6,22 @@ namespace PGP2P_Messenger
     {
         static void Main(string[] args)
         {
-            
+            var p = new Program();
+            p.Init();
+        }
+        public void Init()
+        {
+            Console.WriteLine("Initializing PGP2P Messenger");
+            Console.WriteLine("1. Send message");
+            Console.WriteLine("2. Receive message");
+            Console.WriteLine("Please make a selection:");
+            var k = Console.ReadKey();
+            if(k.Key == ConsoleKey.D1)
+            {
+                Console.Clear();
+                Console.WriteLine("Enter message content:");
+                var mc = Console.ReadLine();
+            }
         }
     }
 }
